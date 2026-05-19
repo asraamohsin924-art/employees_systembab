256899aa00dda2d1671e60fadb20754d78016e4e
asraa <babilsewage.acc@gmail.com>
2026-05-19 12:04:12 +0300

    Fix login access

--- employees/views.py
+++ employees/views.py
@@ -2,8 +2,42 @@ from django.shortcuts import render
 from .models import Employee
 from weasyprint import HTML
 from django.http import HttpResponse
-from django.contrib.auth import logout
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
+import pandas as pd
+from django.core.files.storage import FileSystemStorage
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+def to_int(value):
+    if value is None:
+        return 0
+    value = str(value).strip()
+    if value == '' or value.lower() == 'nan':
+        return 0
+    return int(float(value))
+@staff_member_required
+
+def login_view(request):
+    error = None
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('salary_lookup')
+        else:
+            error = "بيانات الدخول غير صحيحة"
+
+    return render(request, "login.html", {"error": error})
+@login_required
 def salary_lookup(request):
     employee = None
     error = None
@@ -15,8 +49,8 @@ def salary_lookup(request):
             emp_id = emp_id.strip()
 
             employee = Employee.objects.filter(
-                emp_id=str(emp_id)
-            ).first()
+    emp_id=str(emp_id)
+).first()
 
             if not employee:
                 error = "لا يوجد موظف بهذا الرقم"
@@ -28,7 +62,10 @@ def salary_lookup(request):
         "error": error
     })
 
-
+# 👇 هنا تضيفها (تحتها مباشرة)
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 def generate_pdf(request, emp_id):
     employee = Employee.objects.filter(emp_id=emp_id).first()
 
