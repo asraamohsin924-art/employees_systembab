86d195ec1a49d4bfc7721cafa0ce66e53d61c585
asraa <babilsewage.acc@gmail.com>
2026-05-19 10:07:55 +0300

    Change admin URL

--- employees_system/urls.py
+++ employees_system/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # 🔥 هذا السطر مهم جداً
+    path('secret-dashboard-2026/', admin.site.urls),  # 🔥 هذا السطر مهم جداً
     path('', include('employees.urls')),
 ]
