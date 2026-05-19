9d13a5c51321b41c25eb3b39e0e3ab866075a4cb
asraa <babilsewage.acc@gmail.com>
2026-05-19 12:00:01 +0300

    Change admin URL

--- employees/urls.py
+++ employees/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+   
     path('salary/', views.salary_lookup, name='salary_lookup'),
-    
+   
     path('pdf/<str:emp_id>/', views.generate_pdf, name='generate_pdf'),
    # path('upload/', views.upload_excel, name='upload_excel'),
     
